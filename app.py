76b02dabb99281b54903f47652feb77c53695d2a
dch777 <d.chanana777@gmail.com>
2024-03-30 19:36:47 -0400

    Fixed assistant

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from util.assistant import client
+import base64
+from dotenv import load_dotenv
 from flask import Flask, request
 from flask_cors import CORS
-from dotenv import load_dotenv
+import io
 import sys
-import os
+import filetype
 
 load_dotenv()
 
+from util.assistant import *
 
 server = Flask(__name__)
 server.config['CORS_HEADERS'] = 'Content-Type'
@@ -20,15 +22,17 @@ CORS(server, resources={r"/*": {"origins": "*"}})
 @server.route("/create_thread")
 def create_thread():
     thread = client.beta.threads.create()
+    print(run_assistant(thread.id), file=sys.stderr)
     return thread.id
 
 
 @server.route("/messages/<string:thread_id>/send", methods=['POST'])
 def send_message(thread_id):
     data = request.json
-    if "message" in data:
-        raw = base64.b64decode(message[data])
-        out = io.StringIO(raw)
+    if "audio" in data:
+        raw = base64.b64decode(data["audio"].split(",")[1])
+        out = io.BytesIO(raw)
+        out.name = "input.webm"
 
         message = whisper_stt(out)
 
@@ -37,7 +41,12 @@ def send_message(thread_id):
             role="user",
             content=message
         )
-        return "message sent", 200
+
+        response = run_assistant(thread_id)
+        encoded_response = str(base64.b64encode(whisper_tts(response)),
+                               encoding="utf-8")
+
+        return {"response": response, "audio": encoded_response}, 200
     else:
         return "no message provided", 405
 
@@ -45,7 +54,6 @@ def send_message(thread_id):
 @server.route("/messages/<string:thread_id>")
 def get_messages(thread_id):
     thread = client.beta.threads.messages.list(thread_id)
-    print(thread, file=sys.stderr)
     messages = [{"role": message.role, "content": message.content[0].text.value}
                 for message in thread.data]
     return messages, 200
--- util/assistant.py
+++ util/assistant.py
@@ -1,37 +1,53 @@
 from openai import OpenAI
 import io
+import sys
 
 client = OpenAI()
 
 # TODO: Write prompt and functions
-assistant = client.beta.assistants.create(
-    name="Language Learner",
-    instructions="",
-    tools=[],
-    model="gpt-3.5-turbo",
-)
+
+prompt = """You are an expert storyteller and extrovert conversationalist with expert proficiency in English and <language>. Your task is to engage in a conversation with a novice <language> speaker whose native language is English. The conversation should flow naturally and be immersive. Feel free to tell stories, ask the user questions, and respond to the user’s stories with your personal experience. You will also be given the speaker's fluency level on a scale from 1-100. Your goal should be to seamlessly slip words from <language> into the conversation, trying to target easier words if proficiency is lower and more complex if it is higher. The amount of English you use in the conversation should be based on this level. For example if the user is level 1, speak in 99% english.
+
+Make sure to keep the conversation engaging and natural. If the user asks about a word or phrase in <language>, give a quick response to clarify in English, and then continue the conversation in <language>. Each of the things that you say should be between around 2-4 sentences, but 2 in most cases if it is unnecessary for more. Ask at most 2 questions.
+
+Avoid long messages in <language> as this may confuse the user. The language model should act as a friend to have a conversation with. It should prompt the user
+
+Fluency level: 1/100
+Language: Italian
+Begin the conversation with a short introduction of yourself."""
 
 
 def run_assistant(thread_id):
-    client.beta.threads.runs.create(
-        thread_id=thread_id,
-        assistant_id=assistant.id,
-        instructions=""
+    thread = client.beta.threads.messages.list(thread_id)
+    messages = [{"role": message.role, "content": message.content[0].text.value}
+                for message in thread.data]
+    messages.reverse()
+    messages.insert(0, {"role": "system", "content": prompt, })
+
+    print(messages, file=sys.stderr)
+
+    chat_completion = client.chat.completions.create(
+        messages=messages,
+        model="gpt-4-turbo-preview",
     )
+
+    client.beta.threads.messages.create(
+        thread_id=thread_id,
+        role="assistant",
+        content=chat_completion.choices[0].message.content
+    )
+
+    return chat_completion.choices[0].message.content
 
 
 def whisper_tts(text, voice="alloy"):
     response = client.audio.speech.create(
-        model="tts-1",
+        model="tts-1-hd",
         voice=voice,
         input=text
     )
 
-    out = io.StringIO("")
-    for data in response.iter_bytes():
-        out.write(data)
-
-    return out
+    return response.content
 
 
 def whisper_stt(audio_file):
@@ -40,4 +56,4 @@ def whisper_stt(audio_file):
         file=audio_file
     )
 
-    return transcription
+    return transcription.text
