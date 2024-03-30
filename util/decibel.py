53c84036f984bfb7f10fd4faaf8826c26d88cd68
ion05 <49789627+ion05@users.noreply.github.com>
2024-03-30 15:06:17 -0400

    base64 decode

--- util/decibel.py
+++ util/decibel.py
@@ -1,30 +1,23 @@
 # Beat tracking example
 import librosa
-import librosa.display
-import matplotlib.pyplot as plt 
+import numpy as np
+import base64
+import io
+# import librosa.display
+# import matplotlib.pyplot as plt 
 
 # 1. Get the file path to an included audio example
-
-
-def decibelAnalysis():
-    filename = librosa.example('nutcracker')
-    print(filename)
-# 2. Load the audio as a waveform `y`
-#    Store the sampling rate as `sr`
-    y, sr = librosa.load(filename)
-# 3. Run the default beat tracker
-    tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
+base64_audio_string = ""
+with open("encoded-20240330185923.txt", "r") as file:
+    base64_audio_string = file.read()
+audio_bytes = base64.b64decode(base64_audio_string)
+def decibelAnalysis(file):
+    # filename = librosa.example('nutcracker')
+    # print(filename)
+    y, sr = librosa.load(file, sr=None)
     D = librosa.stft(y)
-    D_db = librosa.amplitude_to_db(abs(D))
-
-
-    print('Estimated tempo: {:.2f} beats per minute'.format(tempo))
-    plt.figure(figsize=(10, 5))
-    librosa.display.specshow(D_db, sr=sr, x_axis='time', y_axis='log')
-    plt.colorbar(format='%+2.0f dB')
-    plt.title('Spectrogram')
-    plt.show()
-# 4. Convert the frame indices of beat events into timestamps
-    beat_times = librosa.frames_to_time(beat_frames, sr=sr)
-    pass
-decibelAnalysis()
+    D_db = librosa.amplitude_to_db(D, top_db=None, ref = np.min)
+    print("average decibels: ", np.nanmean(D_db))
+    print("max decibels: ", np.nanmax(D_db))
+    return (np.nanmean(D_db), np.nanmax(D_db))
+decibelAnalysis(io.BytesIO(audio_bytes))   
