19cd501b292a696f4a7c2c6454e0c71aa5c3ffda
xanmankey <86136010+xanmankey@users.noreply.github.com>
2024-03-30 19:09:21 -0400

    added a contrast function

--- util/pace.py
+++ util/pace.py
@@ -8,56 +8,7 @@ import matplotlib.pyplot as plt
 from dotenv import load_dotenv
 from openai.types.audio import Transcription
 
-# load_dotenv()
-# import assistant as whisper
 
-# Faster speech - faster pitch fluctuations
-# Spectral feature analysis
-# def pace(audio: bytes) -> int:
-#     """
-#     Returns pace metrics about the audio.
-#     This includes:
-#     - Average pace throughout the audio file
-#     - Lists any abnormalities in pace
-#     """
-#     # 20hz to 20000hz
-
-#     # Load audio into time series and sampling rate
-#     ts, sr = librosa.load(audio)
-
-#     # Calculate the contrast for each unit of time in the audio
-#     sc = librosa.feature.spectral_contrast(y=ts, sr=sr)
-
-#     # Find the peaks in the spectral contrast
-#     peaks, _ = scipy.signal.find_peaks(sc[0])
-
-#     # Calculate the average spectral contrast
-#     pace = np.diff(peaks).mean()
-
-#     # Calculate the standard deviation
-#     std_dev = np.diff(peaks).std()
-
-#     # Find any abnormalities (more than 2 standard deviations from the mean)
-#     abnormalities = np.where(np.diff(peaks) > pace + 2 * std_dev)[0]
-
-#     # Get the timestamps of the abnormalities
-#     abnormalities = librosa.frames_to_time(abnormalities, sr=sr)
-
-#     print(pace)
-#     print(abnormalities)
-
-#     # Plot the spectral contrast graph
-#     plt.figure()
-#     plt.plot(sc[0])
-#     plt.title("Spectral Contrast")
-#     plt.xlabel("Time")
-#     plt.ylabel("Contrast")
-#     plt.show()
-
-#     return pace, abnormalities
-
-
-# Given a base64 string
 def wpm(file: io.BufferedReader, audio: io.BytesIO) -> dict:
     """
     Returns wpm of the audio.
